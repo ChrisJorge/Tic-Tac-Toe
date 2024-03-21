8e8c92747249a579cdacd3f230dcc36774fe6817
Chris <Christopherjorge47@icloud.com>
2024-03-21 11:30:49 -0400

    Board now shows X's and O's

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-for (i = 0; i < document.querySelectorAll('button').length; i++)
-{
-    document.querySelectorAll('button')[i].addEventListener('click', function(){
-        let press = this.className
-        console.log(press)
-        hasBeenClicked(press)
-    })
-}
-
+turn = 0
 let boardCoodinates = {
     one: true,
     two: true,
@@ -19,14 +11,74 @@ let boardCoodinates = {
     nine: true
 }
 
+
+for (i = 0; i < document.querySelectorAll('button').length; i++)
+{
+    document.querySelectorAll('button')[i].addEventListener('click', function(){
+        let press = this.className
+        hasBeenClicked(press)
+    })
+}
+
+
 let hasBeenClicked = (placement) => {
     this.placement = placement
 
     if(boardCoodinates[`${placement}`] === true){
         boardCoodinates[`${placement}`] = false;
+        placePiece(placement)
     }
     else{
         console.log('already clicked')
     }
 
 }
+
+let placePiece = (placement) => {
+    this.placement = placement
+    let index = 0
+
+    switch (placement){
+        case 'one':
+            index = 0;
+            break;
+        case 'two':
+            index = 1;
+            break;
+        case 'three':
+            index = 2;
+            break;
+        case 'four':
+            index = 3;
+            break;
+        case 'five':
+            index = 4;
+            break;
+        case 'six':
+            index = 5;
+            break;
+        case 'seven':
+            index = 6;
+            break;
+        case 'eight':
+            index = 7;
+            break;
+        case 'nine':
+            index = 8;
+            break;
+        default:
+            console.log(`${placement}?`)
+    }
+    let peice = document.querySelectorAll('button')[index]
+
+    if (turn % 2 === 0){
+        console.log('inside turn % 2 === 0')
+        peice.innerHTML = "X"
+        turn ++;
+    }
+    else{
+        peice.innerHTML = 'O'
+        turn ++
+    }
+}
+
