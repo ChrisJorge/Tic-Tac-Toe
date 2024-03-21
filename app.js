6deb55f77d95f32b7cbfa1ed1110763c36c6a66e
Chris <Christopherjorge47@icloud.com>
2024-03-21 10:56:02 -0400

    Created logic to check if board spot has already been clicked

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-
 for (i = 0; i < document.querySelectorAll('button').length; i++)
 {
     document.querySelectorAll('button')[i].addEventListener('click', function(){
         let press = this.className
         console.log(press)
+        hasBeenClicked(press)
     })
+}
+
+let boardCoodinates = {
+    one: true,
+    two: true,
+    three: true,
+    four: true,
+    five: true,
+    six: true,
+    seven: true,
+    eight: true,
+    nine: true
+}
+
+let hasBeenClicked = (placement) => {
+    this.placement = placement
+
+    if(boardCoodinates[`${placement}`] === true){
+        boardCoodinates[`${placement}`] = false;
+    }
+    else{
+        console.log('already clicked')
+    }
+
 }
