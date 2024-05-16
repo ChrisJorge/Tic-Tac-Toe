73510ccb95b0afe131ec4c38b5ce96ddb67d5447
Chris <Christopherjorge47@icloud.com>
2024-05-15 22:35:18 -0400

    Modified code and added single player option

--- app.js
+++ app.js
@@ -1,186 +1,224 @@
+// Declare necessary variables
+let optionsContainer = document.querySelector('.optionsMasterContainer')  
+let gameContainer = document.querySelector('.gameContainer')
 let title = document.querySelector('.title')
+let restartbtn = document.querySelectorAll('.button')[1]
+let changeplayersbtn = document.querySelectorAll('.button')[0]
 let turn = 0
+let players = 0
 let won = false
-let boardCoodinates = {
-    one: true,
-    two: true,
-    three: true,
-    four: true,
-    five: true,
-    six: true,
-    seven: true,
-    eight: true,
-    nine: true
+let board = {
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
 }
 
-let winCondition = {
-    one: "1",
-    two: "2",
-    three: "3",
-    four: "4",
-    five: "5",
-    six: "6",
-    seven: "7",
-    eight: "8",
-    nine: "9"
+// Create functions 
+
+// Function to make the options container dissapear and the game board appear
+const start = () => {
+    restartbtn.setAttribute('style', 'display: inline-block')
+    restartbtn.addEventListener('click', (() => {
+                announcement("X's Turn")
+                restart()
+    }))
+    changeplayersbtn.setAttribute('style', 'display: inline-block')
+    changeplayersbtn.addEventListener('click', (() => {
+        window.location.reload()
+    }))
+    optionsContainer.setAttribute('style', 'display: none')
+    gameContainer.setAttribute('style', 'display: flex')
+    activateSpaces()
 }
 
-for (i = 0; i < document.querySelectorAll('button').length; i++)
-{
-    document.querySelectorAll('button')[i].addEventListener('click', function(){
-        let press = this.id
-        console.log(press)
-        hasBeenClicked(press)
-    })
+// Function for activating the board spaces
+const activateSpaces = () => {
+    for(let i = 0; i < document.querySelectorAll('.btn').length; i++)
+        {
+            document.querySelectorAll('.btn')[i].addEventListener('click', (event) => {
+              let num = event.target.id
+              position(num)
+            })
+        }
+        announcement("X's turn")
 }
 
+// Function to determine position
+const position = (num) => {
+    let index = -1
+    switch(num)
+    {
+        case '1':
+            index = 0;
+            break;
+        case '2':
+            index = 1;
+            break;
+        case '3':
+            index = 2;
+            break;
+        case '4':
+            index = 3;
+            break;
+        case '5':
+            index = 4;
+            break;
+        case '6':
+            index = 5;
+            break;
+        case '7':
+            index = 6;
+            break;
+        case '8':
+            index = 7;
+            break;
+        case '9':
+            index = 8;
+            break;
+        default:
+            break;
+    }
+    place(index)
+}
+// function to place piece
+const place = (i) => {
+    let spot = document.querySelectorAll('.btn')[i]
+    if(turn % 2 == 0)
+        {
+            spot.innerHTML = "X"
+            board[`${i + 1}`] = "X"
+            spot.disabled = true
+            announcement("O's Turn")
+            turn ++;
+        }
+    else{
+        spot.innerHTML = 'O'
+        board[`${i + 1}`] = "O"
+        announcement("X's Turn")
+        spot.disabled = true
+        turn ++
+    }
+
+    winCheck()
+    if(players === 'one' && turn % 2 != 0 && turn < 8)
+        {
+            robotChoice()
+        }
+    
+}
+
+
+// Function for computer to pick a spot if one player is selected
+const robotChoice = () => {
+    let picked = false
+    while (!picked)
+        {
+            let index = Math.floor(Math.random() * 9)
+            spot = document.querySelectorAll('.btn')[index]
+            if(spot.disabled != true)
+                {
+                    picked = true
+                    place(index)
+                }
+        }
+}
+
+// Function checking for a win
+const winCheck = () => {
+    console.log(board[1])
+    console.log(board[2])
+    if (board[1] === board[2] && board[1] === board[3])
+        {
+            announcement(`${board['1']} wins`);
+            won = true;
+        }
+        else if(board['4'] === board['5'] && board['4'] === board['6'])
+        {
+            announcement(`${board['4']} wins`);
+            won = true;
+        }
+        else if(board['7'] === board['8'] && board['7'] === board['9'])
+        {
+            announcement(`${board['7']} wins`);
+            won = true;
+        }
+        else if(board['1'] === board['5'] && board['1'] === board['9'])
+        {
+            announcement(`${board['1']} wins`);
+            won = true;
+        }
+        else if(board['3'] === board['5'] && board['3'] === board['7'])
+        {
+            announcement(`${board['3']} wins`);
+            won = true;
+        }
+        else if(board['1'] === board['4'] && board['1'] === board['7'])
+        {
+            announcement(`${board['1']} wins`);
+            won = true;
+        }
+        else if(board['2'] === board['5'] && board['2'] === board['8'])
+        {
+            announcement(`${board['2']} wins`);
+            won = true;
+        }
+        else if(board['3'] === board['6'] && board['3'] === board['9'])
+        {
+            announcement(`${board['3']} wins`);
+            won = true;
+        }
+
+        if (won !== true && turn > 8)
+        {
+            announcement('Tie')
+            placeButtons()
+        }
+        else if (won === true)
+            {
+                placeButtons()
+            }
+}
+
+const restart = () => {
+    board = {
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+        }
+    turn = 0
+    won = false
+    for(i = 0; i < document.querySelectorAll('.btn').length; i++)
+        {
+            let btn = document.querySelectorAll('.btn')[i]
+            btn.innerHTML = ''
+            btn.disabled = false
+        }
+}
+
+
+// Function to update title
 const announcement = (message) => 
     {
         title.innerHTML = message
     }
 
 
-const hasBeenClicked = (placement) => {
-    this.placement = placement
-
-    if(boardCoodinates[`${placement}`] === true){
-        boardCoodinates[`${placement}`] = false;
-        placePiece(placement)
-    }
-    else{
-        announcement('Space already clicked, choose another one')
-    }
-
-}
-
-const placePiece = (placement) => {
-    this.placement = placement
-    let index = 0
-
-    switch (placement){
-        case 'one':
-            index = 0;
-            break;
-        case 'two':
-            index = 1;
-            break;
-        case 'three':
-            index = 2;
-            break;
-        case 'four':
-            index = 3;
-            break;
-        case 'five':
-            index = 4;
-            break;
-        case 'six':
-            index = 5;
-            break;
-        case 'seven':
-            index = 6;
-            break;
-        case 'eight':
-            index = 7;
-            break;
-        case 'nine':
-            index = 8;
-            break;
-        default:
-            console.log(`${placement}?`)
-            break;
-    }
-    
-    let peice = document.querySelectorAll('button')[index]
-
-    if (turn % 2 === 0){
-        peice.innerHTML = "X"
-        winCondition[`${placement}`] = 'X'
-        turn ++;
-        announcement("O's Turn")
-        winCheck();
-    }
-    else{
-        peice.innerHTML = 'O'
-        winCondition[`${placement}`] = 'O'
-        turn ++
-        announcement("X's Turn")
-        winCheck();
-    }
-}
-
-const winCheck =() => {
-
-    if (winCondition['one'] === winCondition['two'] && winCondition['one'] === winCondition['three'])
+for(let i = 0; i < document.querySelectorAll('.choice').length; i++)
     {
-        announcement(`${winCondition['one']} wins`);
-        won = true;
+        document.querySelectorAll('.choice')[i].addEventListener('click', (event) => {
+             players = event.target.id
+            start()
+        })
     }
-    else if(winCondition['four'] === winCondition['five'] && winCondition['four'] === winCondition['six'])
-    {
-        announcement(`${winCondition['four']} wins`);
-        won = true;
-    }
-    else if(winCondition['seven'] === winCondition['eight'] && winCondition['seven'] === winCondition['nine'])
-    {
-        announcement(`${winCondition['seven']} wins`);
-        won = true;
-    }
-    else if(winCondition['one'] === winCondition['five'] && winCondition['one'] === winCondition['nine'])
-    {
-        announcement(`${winCondition['one']} wins`);
-        won = true;
-    }
-    else if(winCondition['three'] === winCondition['five'] && winCondition['three'] === winCondition['seven'])
-    {
-        announcement(`${winCondition['three']} wins`);
-        won = true;
-    }
-    else if(winCondition['one'] === winCondition['four'] && winCondition['one'] === winCondition['seven'])
-    {
-        announcement(`${winCondition['one']} wins`);
-        won = true;
-    }
-    else if(winCondition['two'] === winCondition['five'] && winCondition['two'] === winCondition['eight'])
-    {
-        announcement(`${winCondition['two']} wins`);
-        won = true;
-    }
-    else if(winCondition['three'] === winCondition['six'] && winCondition['three'] === winCondition['nine'])
-    {
-        announcement(`${winCondition['three']} wins`);
-        won = true;
-    }
-
-    if (won !== true && turn > 8){
-        announcement('Tie')
-        disabled()
-        let container = document.querySelector('h1');
-        btn = document.createElement('button');
-        btn.setAttribute('class', 'show-btn');
-        btn.setAttribute('onClick', 'reset()');
-        btn.textContent = "Replay"
-        container.appendChild(btn);
-        
-    }
-    else if(won === true){
-        disabled()
-        let container = document.querySelector('h1');
-        btn = document.createElement('button');
-        btn.setAttribute('class', 'show-btn');
-        btn.setAttribute('onClick', 'reset()');
-        btn.textContent = "Replay"
-        container.appendChild(btn);
-        
-    }  
-}
-
-const disabled = () => {
-    for(i = 0; i < document.querySelectorAll('btn').length; i++){
-        let btn = document.querySelectorAll('btn')[i]
-        btn.disabled = true
-    }
-}
-
-const reset = () => {
-    window.location.reload()
-}
